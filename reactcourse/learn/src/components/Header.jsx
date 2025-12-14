4e9552021d9febd8d3ecb2fb25ce4a8a1715f4b6
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-12-14 23:53:36 +0530

    learn

--- reactcourse/learn/src/components/Header.jsx
+++ reactcourse/learn/src/components/Header.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/Header.scss';
 import { Close, MenuBookOutlined } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
+
+
 const Header = () => {
+    const [active, setActive] = useState(false);
+    const showMenu = () => {
+        setActive(!active);
+    }
     return (
         <div className="header">
             <div className="header__logo">
                 <h1>Vrushali A Poojary</h1>
             </div>
 
-            <nav>
+            <nav className="navbar">
                 <ul>
                     <div className="closed">
-                        <Close className="close" />
+                        <Close className="close" onClick={showMenu}/>
                     </div>
 
                     <li>
-                        <Link to="/"><b>Home</b></Link>
+                        <Link to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/"><b>About</b></Link>
+                        <Link to="/">About</Link>
                     </li>
                     <li>
-                        <Link to="/"><b>Portfolio</b></Link>
+                        <Link to="/">Portfolio</Link>
                     </li>
                     <li>
-                        <Link to="/"><b>Blog</b></Link>
+                        <Link to="/">Blog</Link>
                     </li>
                     <li>
-                        <Link to="/"><b>Contact</b></Link>
+                        <Link to="/">Contact</Link>
                     </li>
-
                 </ul>
             </nav>
+
             <div className="changer">
                 <MenuBookOutlined className="menu" />
             </div>
