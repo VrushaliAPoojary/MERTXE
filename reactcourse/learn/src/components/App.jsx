4e6e152477c8c18972009ce6975fe6eeb414d105
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-12-17 00:30:48 +0530

    section set

--- reactcourse/learn/src/components/App.jsx
+++ reactcourse/learn/src/components/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import './styles/App.scss';
 import Header from "./Header";
 import Main from "./Main";
+import Section from "./Section";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div className="App">
        {/* <Header /> */}
-      <Main />
+      {/* <Main /> */}
+      <Section/>
+      {/* <Contact/> */}
     </div>
   );
 }
