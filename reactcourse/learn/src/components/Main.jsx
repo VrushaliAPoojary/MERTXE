1b212faabc27e8811f5aaa3b70a972c588151e14
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-12-16 18:24:43 +0530

    main css set

--- reactcourse/learn/src/components/Main.jsx
+++ reactcourse/learn/src/components/Main.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import './styles/Main.scss';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 
 const Main = () => {
   return (
@@ -9,8 +13,15 @@ const Main = () => {
                 
             <div className="text">
                 <p>Hello Everyone !! </p>
-                <h1>I am Vrushali A Poojary</h1>
+                <h1>I am <span className="color"> Vrushali A Poojary</span></h1>
                 <p>Front-End Developer</p>
+
+                <div className="icons">
+                    <TwitterIcon className="icon"/>
+                    <LinkedInIcon className="icon"/>
+                    <GitHubIcon className="icon"/>
+
+                </div>
             </div>
          </div>
         </div>
