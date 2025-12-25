ed35dc7db6177739125ad42df9a838ef8deef5c5
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-12-25 19:42:58 +0530

    useeffectforlocationset

--- reactcourse/learn/src/components/App.jsx
+++ reactcourse/learn/src/components/App.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./Header";
 import Main from "./Main";
 import Section from "./Section";
 import Contact from "./Contact";
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0,0);
+  },[location.pathname]);
   return (
     <div className="App">
       <Header />
