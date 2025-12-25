4bd0ca60c995ba15aa2c7495c3b660afd8bf3957
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-12-25 12:31:10 +0530

    contact

---
+++ reactcourse/learn/src/components/Contact.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ArrowBack } from "@mui/icons-material";
+import './styles/Contact.scss';
+import {Link} from 'react-router-dom';
+
+const Contact=() =>{
+    return(
+        <div className="contact">
+            <Link to ='/'>
+             <ArrowBack className='arrow'/>
+            </Link>
+            <div className="inputs">
+                <h1>Contact Me</h1>
+                <input type="text" placeholder="Name"/>
+                <input type="email" placeholder="Email"/>
+                <input type="text" placeholder="message" className="message"/>
+                <button>Push</button>
+            </div>
+
+        </div>
+    )
+}
+export default Contact;
